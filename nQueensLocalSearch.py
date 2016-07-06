3d10e10b5d12816a09dd5f6226cc9a91c2fee931
jplourenco <jplourenco@users.noreply.github.com>
2016-07-07 00:02:18 +0100

    Update nQueensLocalSearch.py

--- nQueensLocalSearch.py
+++ nQueensLocalSearch.py
@@ -6,33 +6,24 @@ import random as rd
 
 
 def testSolution(sol):
-    isSolution = True
     for k in range(len(sol)-1):
-        if(not isSolution):
-            break;
         for i in range(k+1,len(sol)):
             if(sol[k] == sol[i]+(i-k) or sol[k] == sol[i]-(i-k)):
-                #print(i,sol[i])
-                isSolution = False
-                break
-    if(isSolution):
-        return 1,sol," is a solution!"
-    else:
-        return 0,sol," is not a solution!"
+                return False
+    return True
 
 def iterations(sol,n):
 
     for j in range(n):
-        #print testSolution(sol)
-        if(testSolution(sol)[0]==0):
+        if(testSolution(sol)):
             rd.shuffle(sol)
         else:
             return j,sol
-    return "No solution found in ",n," iterations"
+    return("No solution found in ",n," iterations")
 
 n = int(sys.argv[1])
 it = int(sys.argv[2])
 
 st = range(n)
 
-print iterations(st,it)
+print(iterations(st,it))
