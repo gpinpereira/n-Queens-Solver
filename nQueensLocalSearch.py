21cf6d54f6ae96e060c2483790e8a3e562be6b69
gpinpereira <guipinper@gmail.com>
2016-07-07 19:03:18 +0100

    PEP8
    PEP8

--- nQueensLocalSearch.py
+++ nQueensLocalSearch.py
@@ -10,15 +10,36 @@ def testSolution(sol):
             if(sol[k] == sol[i]+(i-k) or sol[k] == sol[i]-(i-k)):
                 return False
     return True
+    
+
 
 def iterations(sol,n):
-
     for j in range(n):
-        if(testSolution(sol)):           
+        if(testSolution(sol)): 
+            draw_chessboard(sol)
             return j,sol
         else:
             rd.shuffle(sol)
     return "No solution found in " + str(n) +" iterations"
+    
+    
+def draw_chessboard(sol):
+    str = ""
+    for m in range(len(sol)):
+        str=str + "__"    
+    str=str + "\n"    
+    
+    for i,k in enumerate(sol): #draws one line
+         for m in range(len(sol)): 
+            str=str + "|"
+            if m+1==k: 
+                str=str + "Q"
+            else: 
+                str=str + "_"
+         str=str + "|"
+         print(str)
+         str = ""
+    
 
 
 it = int(sys.argv[2])
@@ -26,30 +47,3 @@ st = list(range(int(sys.argv[1])))
 
 
 print iterations(st,it)
-
-
-a = iterations(st,it)
-ar=[0 for i in range(int(sys.argv[1]))]
-str = ""
-
-
-if isinstance(a, basestring ) == False: 
-    a,b = iterations(st,it)
-    print(b)    
-    ar=b
-
-for m in range(len(ar)):
-    str = str + "__"    
-str = str + "\n" 
-
-#cria 1 linha de cada vez
-for i,k in enumerate(ar):
-#cria as parcelas de cada casa. Se a posicao dessa parcela for igual ao valor que esta no arra mete um Q senao e vazia 
-    for j in range(len(ar)):        
-        str = str + "|"
-        # j+i para "sincronizar o tamanho do array com a posicao maxima a que a rainha pode estar"
-        if j+1 == k: str = str + "Q"
-        else: str = str + "_"
-    str = str + "|"
-    print(str)
-    str = ""
